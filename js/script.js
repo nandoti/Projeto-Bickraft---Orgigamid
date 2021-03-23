71caecbcf2d0dbbf0a9d15eebcfb4d7547dd6b81
Fernando Tavares <73204469+nandoti@users.noreply.github.com>
2021-03-23 19:39:43 -0300

    :up:
    Atualização

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Verificação no JS para não bugar
+
+if(window.SimpleSlide) {
+new SimpleSlide({
+  slide: "quote", // nome do atributo data-slide="principal"
+  time: 5000, // tempo de transição dos slides
+});
+new SimpleSlide({
+  slide: "portfolio", 
+  time: 5000, 
+  nav: true
+});
+}
+
+
